041b607936c73b07559c78ae45aa21867379d89d
gyudisanyi <gyudisanyi@gmail.com>
2021-03-21 16:16:55 +0100

    rc-progress replaced with tailor made canvas progress bar holding the same functionality so far

--- compare-sounds/src/App2.js
+++ compare-sounds/src/App2.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Button from './components/Button/Button';
 import { Line } from 'rc-progress';
-import { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import TrackInfo from './components/TrackInfo/TrackInfo';
 import './App.css';
@@ -17,10 +16,30 @@ function App() {
 
   const [trackNodes, setTrackNodes] = useState();
   const [nowPlaying, setNowPlaying] = useState(0);
+  const [progressBarCtx, setProgressBarCtx] = useState();
   const [progress, setProgress] = useState(0);
   const [paused, setPaused] = useState(true);
+  const [loopBarCtx, setLoopBarCtx] = useState();
   const [looping, setLooping] = useState(false);
-  const [loop, setLoop] = useState([0, 1000])
+  const [loop, setLoop] = useState([0, 1000]);
+  const [loopBarClicked, setLoopBarClicked] = useState(false);
+  const [loopBarClickedValue, setLoopBarClickedValue] = useState();
+  
+  const loopBarRef = useCallback((setCanvas) => {
+    if (!setCanvas) {console.log(`nocanvas`); return}
+    const canvasCtx = setCanvas.getContext("2d");
+    canvasCtx.fillStyle = '#333';
+    canvasCtx.fillRect(0, 0, 300, 10);
+    setLoopBarCtx(canvasCtx);
+  }, []);
+
+  const progressBarRef = useCallback((setCanvas) => {
+    if (!setCanvas) {console.log(`nocanvas`); return}
+    const canvasCtx = setCanvas.getContext("2d");
+    canvasCtx.fillStyle = '#333';
+    canvasCtx.fillRect(0, 0, 300, 10);
+    setProgressBarCtx(canvasCtx);
+  }, []);
 
   const trackNodesRef = useCallback((setNode) => {
     if (!setNode) { console.log(`ohno`); return }
@@ -31,6 +50,23 @@ function App() {
     })
     setTrackNodes(Array.from(setNode.children));
   }, []);
+  
+  useEffect(() => {
+    if (!progressBarCtx) { console.log('whatprogresscanvas'); return }
+    progressBarCtx.fillStyle = 'black';
+    progressBarCtx.fillRect(0, 0, 300, 10);
+    progressBarCtx.fillStyle = 'white';
+    progressBarCtx.fillRect(progress*3, 0, 1, 10);
+
+  }, [progress])
+  
+  useEffect(() => {
+    if (!loopBarCtx) { console.log('whatcanvas'); return }
+    loopBarCtx.fillStyle = 'black';
+    loopBarCtx.fillRect(0, 0, 300, 10);
+    loopBarCtx.fillStyle = looping ? 'yellow' : '#333';
+    loopBarCtx.fillRect(loop[0] / 3.33, 0, (loop[1] - loop[0]) / 3.33, 10);
+  }, [loop, looping])
 
   useEffect(() => {
     if (!looping) return;
@@ -58,12 +94,35 @@ function App() {
     let seekSeconds = (e.nativeEvent.offsetX / 300)*trackNodes[0].duration
     trackNodes[0].currentTime=seekSeconds;
   }
+
+  function loopBarMouse(e) {
+    setLoopBarClicked(true);
+    setLoopBarClickedValue(e.nativeEvent.offsetX);
+  }
+
+  function loopBarMouseLeave(e) {
+    if (!loopBarClicked) return;
+    setLoopBarClicked(false);
+    if (Math.abs(loopBarClickedValue - e.nativeEvent.offsetX) < 10) {
+      setLooping(prev => !prev);
+      return;
+    };
+    const newCoord = Math.max(Math.min((e.nativeEvent.offsetX * 10/3), 1000), 0);
+    const newLoop = [loopBarClickedValue * 10/3, newCoord].sort((a, b) => {return a-b});
+    setLoop(newLoop);
+  }
   
-  function switchTrack(i) {
-    console.log(i);
+  function switchTrack({target}) {
+    console.log(target.id);
     trackNodes[nowPlaying].muted = true;
-    trackNodes[(nowPlaying + 1) % trackNodes.length].muted = false;
-    setNowPlaying(prev => (prev + 1) % trackNodes.length);
+    switch (target.id) {
+      case 'switch': trackNodes[(nowPlaying + 1) % trackNodes.length].muted = false;
+        setNowPlaying(prev => (prev + 1) % trackNodes.length);
+        break;
+      default: 
+        trackNodes[target.parentElement.id].muted = false;
+        setNowPlaying(parseInt(target.parentElement.id))
+    }
   }
 
   return (
@@ -74,17 +133,20 @@ function App() {
           <Button id="loop" buttonText="Loop" buttonClass={!looping ? "emptyButton" : ""} handleClick={() => setLooping(prev => !prev)} />
           <Button id="switch" buttonText="Switch" handleClick={switchTrack} />
         </div>
-        <Line strokeLinecap="square" percent={progress} strokeWidth="5" strokeColor="#ffffff" onClick={seek} />
-        <Range
-          min={0}
-          max={1000}
-          defaultValue={[0, 1000]}
-          onChange={(e)=>setLoop(e)}
-          />
+        <canvas ref={progressBarRef}
+        onMouseDown={seek}
+        width={300} height={10} />
+        <canvas ref={loopBarRef}
+        onMouseDown={loopBarMouse}
+        onMouseUp={loopBarMouseLeave}
+        onMouseLeave={loopBarMouseLeave}        
+        width={300} height={10} />
+        {loopBarClicked ? `loopbar held` : `loopbar not held`}
+        {` ${loop[0]} -- ${loop[1]}`}
       </div>
       <div id="tracks">
         <div id="tracksload" ref={trackNodesRef}>{set.map((t, i)=> (<audio src={t[0]} key={i} id={i} loop muted/>))}</div>
-        <div id="tracklist">{set.map((t, i)=> (<TrackInfo handleClick={switchTrack} track={t} key={i} id={i} active={nowPlaying === i}/>))}</div>
+        <div onClick={switchTrack} id="tracklist">{set.map((t, i)=> (<TrackInfo track={t} key={i} id={i} active={nowPlaying === i}/>))}</div>
         <div id="trackdescr">{set[nowPlaying][2]}</div>
       </div>
     </div>
--- compare-sounds/src/components/TrackInfo/TrackInfo.js
+++ compare-sounds/src/components/TrackInfo/TrackInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TrackInfo = ({ track, id, active }) => {
   return (
-    <div className={active ? "active" : "inactive"}>
+    <div id={id} className={active ? "active" : "inactive"}>
       <span className="number">{id + 1}</span>
       <span className="title">{track[1]}</span>
     </div>
