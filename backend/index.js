1ab690312ba14dd83cc00f0001e7619279be9333
gyudisanyi <gyudisanyi@gmail.com>
2021-04-20 14:58:36 +0200

    https local

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
+import fs from 'fs';
+import https from 'https';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 import errorHandler from './middlewares/error-handler.js';
 const app = express();
 const port = 8080;
+
+const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+
+app.get("*", function (req, res, next) {
+
+  if ("https" !== req.headers["x-forwarded-proto"] && "production" === process.env.NODE_ENV) {
+      res.redirect("https://" + req.hostname + req.url);
+  } else {
+      // Continue to other routes if we're not redirecting
+      next();
+  }
+
+});
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +31,8 @@ app.use(router);
 
 app.use(errorHandler);
 
-app.listen(port, () => {
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
   console.log(`Backend server running on port: ${port}`);
 });
